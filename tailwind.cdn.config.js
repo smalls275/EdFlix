c90a49ecdd7426908fe46d48bee26e389ed62090
smalls275 <smalls275@users.noreply.github.com>
2026-05-22 16:58:31 -0400

    Fire TV optimizations: precompile Tailwind, focus styles, debounced search, smaller hero, mute trailer, lighter loadUpcoming, dead CSS removed

---
+++ tailwind.cdn.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'edflix-red': '#E50914',
+        'edflix-dark': '#141414',
+        'edflix-gray': '#181818',
+        'edflix-light-gray': '#2F2F2F',
+      },
+    },
+  },
+  plugins: [],
+}
